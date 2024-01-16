792db24046c4b886b032e22792129c5f1089e59b
Dongseok Kang <dsk08208@gmail.com>
2024-01-16 22:54:05 +0900

    Chore: 개발 초기 세팅 (#2)
    * Chore: submodule 추가 및 yml 세팅
    * Chore: reseponse, exception 세팅
    * Chore: slack webhook 연결 및 threadpool 추가
    * Chore: Excetion 발생 시 slack으로 message 전송
    * Feat: healthcheck api
    
    #1

--- src/main/java/com/fubao/dearbao/DearbaoApplication.java
+++ src/main/java/com/fubao/dearbao/DearbaoApplication.java
@@ -2,8 +2,10 @@ package com.fubao.dearbao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DearbaoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/fubao/dearbao/global/common/exception/ErrorResponse.java
@@ -0,0 +1,29 @@
+package com.fubao.dearbao.global.common.exception;
+
+import com.fubao.dearbao.global.common.response.ResponseDto;
+
+public class ErrorResponse extends ResponseDto {
+    private ErrorResponse(ResponseCode errorCode) {
+        super(false, errorCode.getCode(), errorCode.getMessage());
+    }
+
+    private ErrorResponse(ResponseCode errorCode, Exception e) {
+        super(false, errorCode.getCode(), errorCode.getMessage(e));
+    }
+
+    private ErrorResponse(ResponseCode errorCode, String message) {
+        super(false, errorCode.getCode(), errorCode.getMessage(message));
+    }
+
+    public static ErrorResponse of(ResponseCode errorCode) {
+        return new ErrorResponse(errorCode);
+    }
+
+    public static ErrorResponse of(ResponseCode errorCode, Exception e) {
+        return new ErrorResponse(errorCode, e);
+    }
+
+    public static ErrorResponse of(ResponseCode errorCode, String message) {
+        return new ErrorResponse(errorCode, message);
+    }
+}
