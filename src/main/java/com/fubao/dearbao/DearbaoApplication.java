2e312b069276136b52e9a680246ae093b7387ced
peter <97747863+wcorn@users.noreply.github.com>
2024-01-16 10:46:01 +0900

    Init: project

---
+++ src/main/java/com/fubao/dearbao/DearbaoApplication.java
@@ -0,0 +1,13 @@
+package com.fubao.dearbao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearbaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DearbaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fubao/dearbao/DearbaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fubao.dearbao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearbaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
