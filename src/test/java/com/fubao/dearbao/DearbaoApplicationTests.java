6893ced0dabeedb18871caeb09c7a98f6afff8fb
dongseok <wcorn@dongseokui-MacBookPro.local>
2024-01-25 13:00:30 +0900

    Fix: springboot test set class name

--- src/test/java/com/fubao/dearbao/DearbaoApplicationTests.java
+++ src/test/java/com/fubao/dearbao/DearbaoApplicationTests.java
@@ -3,7 +3,7 @@ package com.fubao.dearbao;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DearbaoApplicationTests.class)
 class DearbaoApplicationTests {
 
     @Test
--- src/test/java/com/fubao/dearbao/domain/HealthCheckControllerTest.java
+++ src/test/java/com/fubao/dearbao/domain/HealthCheckControllerTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(classes = HealthCheckControllerTest.class)
 @AutoConfigureMockMvc
 class HealthCheckControllerTest {
     @Autowired
