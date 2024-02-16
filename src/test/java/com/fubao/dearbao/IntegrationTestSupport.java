0c00313fc7d9c8a1e377219a365525e20849e454
wcorn <dsk08208@gmail.com>
2024-02-17 02:04:45 +0900

    feat: slack 비동기가 처리 없고 메세지만 보내는 기능 추가
    서버에서 스케줄링 처리할때 발생하는 예외를 위해 HttpServletRequest 매개 변수 없는 기능 추가

--- src/test/java/com/fubao/dearbao/IntegrationTestSupport.java
+++ src/test/java/com/fubao/dearbao/IntegrationTestSupport.java
@@ -6,11 +6,13 @@ import com.fubao.dearbao.domain.member.MemberRepository;
 import com.fubao.dearbao.domain.mission.MemberMissionRepository;
 import com.fubao.dearbao.domain.mission.MissionRepository;
 import com.fubao.dearbao.global.util.DateUtil;
+import com.fubao.dearbao.global.util.SlackWebhookUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public abstract class IntegrationTestSupport {
@@ -26,6 +28,8 @@ public abstract class IntegrationTestSupport {
     protected MemberMissionRepository memberMissionRepository;
     @Autowired
     protected DateUtil dateUtil;
+    @MockBean
+    protected SlackWebhookUtil slackWebhookUtil;
     @AfterEach
     void tearDown() {
         memberMissionRepository.deleteAllInBatch();
