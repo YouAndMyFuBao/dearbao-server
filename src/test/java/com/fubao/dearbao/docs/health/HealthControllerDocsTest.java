d095488693bf46b39b1f79cadd1db6655e0bdd2f
wcorn <dsk08208@gmail.com>
2024-02-18 20:44:28 +0900

    docs: health check api docs
     #28

---
+++ src/test/java/com/fubao/dearbao/docs/health/HealthControllerDocsTest.java
@@ -0,0 +1,33 @@
+package com.fubao.dearbao.docs.health;
+
+import com.fubao.dearbao.api.controller.healthcheck.HealthCheckController;
+import com.fubao.dearbao.docs.RestDocsSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class HealthControllerDocsTest extends RestDocsSupport {
+
+    @Override
+    protected Object initController() {
+        return new HealthCheckController();
+    }
+
+    @DisplayName("서버 구동 확인 API")
+    @Test
+    void hello() throws Exception {
+        mockMvc.perform(
+                get("")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("health-check",
+                responseBody()
+            ));
+    }
+}
